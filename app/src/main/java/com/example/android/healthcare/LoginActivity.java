a6783083ac5cf678081ab008a31b8cb9054af9dc
Harsh952000 <95995432+Harsh952000@users.noreply.github.com>
2023-06-11 03:21:15 +0530

    third commit

--- app/src/main/java/com/example/android/healthcare/LoginActivity.java
+++ app/src/main/java/com/example/android/healthcare/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.android.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -34,6 +35,13 @@ public class LoginActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(),"Please Fill All Details",Toast.LENGTH_SHORT).show();
                 else
                     Toast.makeText(getApplicationContext(),"Login Success",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
             }
         });
     }
--- app/src/main/java/com/example/android/healthcare/RegisterActivity.java
+++ app/src/main/java/com/example/android/healthcare/RegisterActivity.java
@@ -2,13 +2,108 @@ package com.example.android.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText edUsername,edPassword,edEmail,edConfirmPassword;
+    Button register;
+    TextView tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        edUsername=findViewById(R.id.editTextRegUserName);
+        edPassword=findViewById(R.id.editTextRegPassword);
+        edEmail=findViewById(R.id.editTextRegEmail);
+        edConfirmPassword=findViewById(R.id.editTextRegConfirmPassword);
+        register=findViewById(R.id.buttonRegister);
+        tv=findViewById(R.id.textViewExistingUser);
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username=edUsername.getText().toString();
+                String email=edEmail.getText().toString();
+                String password=edPassword.getText().toString();
+                String confirm=edConfirmPassword.getText().toString();
+
+
+
+                if(username.length()==0 || email.length()==0 || password.length()==0 || confirm.length()==0)
+                    Toast.makeText(RegisterActivity.this, "Please Fill The Details", Toast.LENGTH_SHORT).show();
+                else
+                {
+                    if(password.compareTo(confirm)==0)
+                    {
+                        if(isValid(password))
+                        {
+
+                            Toast.makeText(RegisterActivity.this, "Register SucessFully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+                        }
+                        else
+                        {
+                            Toast.makeText(RegisterActivity.this, "Password must contain at least 8 characters ,haing letter,digit and special symbol", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                    else
+                    {
+                        Toast.makeText(RegisterActivity.this, "Password and confirm Password didn't match", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+
+            }
+        });
+    }
+    public static boolean isValid(String passwordhere)
+    {
+        int f1=0,f2=0,f3=0;
+        if(passwordhere.length()<8)
+                return false;
+        else
+        {
+            for(int p=0;p<passwordhere.length();p++)
+            {
+                if(Character.isLetter((passwordhere.charAt(p))))
+                    f1=1;
+
+            }
+            for(int r=0;r<passwordhere.length();r++)
+            {
+                if(Character.isDigit((passwordhere.charAt(r))))
+                    f2=1;
+
+            }
+            for(int s=0;s<passwordhere.length();s++)
+            {
+                char ch=passwordhere.charAt(s);
+                if(ch>=33 && ch<=46 || ch==64)
+                    f3=1;
+
+            }
+
+        }
+        if(f1==1 && f2==1 && f3==1)
+            return true;
+        else
+            return false;
+
     }
 }
